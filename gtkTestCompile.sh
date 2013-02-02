49774156e4f57d38fbdac06edb09bbdea7dc8c80
Harry van Haaren <harryhaaren@gmail.com>
2013-02-02 01:24:03 +0000

    -Added Modulation wheel -> LFO speed routing in FAUST dsp. Fixed gtkTestCompile.sh

--- gtkTestCompile.sh
+++ gtkTestCompile.sh
@@ -4,6 +4,6 @@ cd faust
 
 faust -cn source -a jack-gtk.cpp main.dsp -o main.cpp
 
-g++ '-I/usr/lib/faust/' -DPLUGIN_URI='"http://www.openavproductions.com/source"'   main.cpp `pkg-config --cflags --libs jack gtk+-2.0` -lsndfile -o source
+g++ '-I/usr/lib/faust/' -DPLUGIN_URI='"http://www.openavproductions.com/sorcer"'   main.cpp `pkg-config --cflags --libs jack gtk+-2.0` -lsndfile -o sorcer
 
-./source
+./sorcer
