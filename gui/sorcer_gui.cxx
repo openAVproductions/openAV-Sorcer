0a45ebfd52293b4e70ac17346d3977d9c7249a34
Harry van Haaren <harryhaaren@gmail.com>
2013-05-08 15:54:29 +0100

    -Commented prints, now seems ready for release

--- gui/sorcer_gui.cxx
+++ gui/sorcer_gui.cxx
@@ -35,25 +35,25 @@ typedef struct {
 
 static GtkWidget* make_gui(SorcerGui *self) {
     
-    cout << "Init GTKMM!" << endl;
+    //cout << "Init GTKMM!" << endl;
     Gtk::Main* main = Gtk::Main::instance(); // _gtkmm_internals // for QT hosts
     
     main->init_gtkmm_internals();
     
     if( !Glib::thread_supported() )
     {
-      cout << "calling Glib::thread_init()" << endl;
+      //cout << "calling Glib::thread_init()" << endl;
       Glib::thread_init();
     }
     
-    cout << "create container!" << endl;
+    //cout << "create container!" << endl;
     // Return a pointer to a gtk widget containing our GUI
     GtkWidget* container = gtk_vbox_new(FALSE, 2);
     
-    cout << "new canvast!" << endl;
+    //cout << "new canvast!" << endl;
     self->widget = new Canvas();
     
-    cout << "adding to window!" << endl;
+    //cout << "adding to window!" << endl;
     // get the gobject, and add that to the container
     gtk_container_add((GtkContainer*)container, (GtkWidget*)self->widget->gobj() );
     
@@ -76,7 +76,7 @@ static LV2UI_Handle instantiate(const struct _LV2UI_Descriptor * descriptor,
     
     SorcerGui* self = (SorcerGui*)malloc(sizeof(SorcerGui));
     
-    cout << "Allocated SourceGUI!" << endl;
+    //cout << "Allocated SourceGUI!" << endl;
     
     if (self == NULL) return NULL;
     
@@ -84,26 +84,26 @@ static LV2UI_Handle instantiate(const struct _LV2UI_Descriptor * descriptor,
     {
       if (!strcmp(features[i]->URI, LV2_URID__map))
       {
-        cout << "Found feature URID map!" << endl;
+        //cout << "Found feature URID map!" << endl;
         //self->guiState->map = (LV2_URID_Map*)features[i]->data;
       }
     }
     
-    cout << "Creating UI!" << endl;
+    //cout << "Creating UI!" << endl;
     *widget = (LV2UI_Widget)make_gui(self);
     
-    cout << "Writing controller f(x)!" << endl;
+    //cout << "Writing controller f(x)!" << endl;
     
     self->widget->controller = controller;
     self->widget->write_function = write_function;
     
-    cout << "returning..." << endl;
+    //cout << "returning..." << endl;
     
     return (LV2UI_Handle)self;
 }
 
 static void cleanup(LV2UI_Handle ui) {
-    printf("cleanup()\n");
+    //printf("cleanup()\n");
     SorcerGui *pluginGui = (SorcerGui *) ui;
     free(pluginGui);
 }
@@ -116,12 +116,12 @@ static void port_event(LV2UI_Handle ui,
 {
     SorcerGui *self = (SorcerGui *) ui;
     
-    cout << "Port event on index " << port_index << "  Format is " << format << endl;
+    //cout << "Port event on index " << port_index << "  Format is " << format << endl;
     
     if ( format == 0 )
     {
       float tmp = *(float*)buffer;
-      cout << "Port " << port_index << " gets " << tmp << endl;
+      //cout << "Port " << port_index << " gets " << tmp << endl;
       self->widget->values[port_index] = tmp;
       self->widget->redrawUpdate();
     }
@@ -134,7 +134,7 @@ static LV2UI_Descriptor descriptors[] = {
 };
 
 const LV2UI_Descriptor * lv2ui_descriptor(uint32_t index) {
-    printf("lv2ui_descriptor(%u) called\n", (unsigned int)index); 
+    //printf("lv2ui_descriptor(%u) called\n", (unsigned int)index); 
     if (index >= sizeof(descriptors) / sizeof(descriptors[0])) {
         return NULL;
     }
